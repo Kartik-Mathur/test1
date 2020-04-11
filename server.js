ea6cb1a619f75bd952f6ffc2bf40fb67b6b2a503
Kartik Mathur <kartik.mathur@codingblocks.com>
2020-04-11 16:56:08 +0530

    Add Path Hi

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ app.get('/',(req,res)=>{
     res.send('Hello World!')
 })
 
+app.get('/hi',(req,res)=>{
+    res.send('Hello')
+})
+
 const PORT = process.env.PORT||3333
 app.listen(PORT,()=>{
     console.log('server started '+ PORT)
